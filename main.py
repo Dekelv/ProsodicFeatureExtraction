dae945b7ae97ffa7ffe1949ad1774cbf90c8a0cc
Dekelv <id.viner@gmail.com>
2020-12-01 13:43:55 +0100

    audio segmentizer

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import myprosody as mp
+from pydub import AudioSegment
 p = ""
 c = ""
 
+t1 = t1 * 1000 #Works in milliseconds
+t2 = t2 * 1000
+newAudio = AudioSegment.from_wav("oldSong.wav")
+newAudio = newAudio[t1:t2]
