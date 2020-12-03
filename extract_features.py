dfa154912959a1b8415da90df21137c539ad2f5d
Dekelv <id.viner@gmail.com>
2020-12-03 09:28:51 +0100

    Still not working

--- extract_features.py
+++ extract_features.py
@@ -1,16 +1,22 @@
+#mysp = __import__("my-voice-analysis")
 import myprosody as mysp
+import pickle
 
-p = "SegmentsLesson1/audioSeg-1"
+p = "audioSeg-"
 
 #Insert your local directory address here to work with myprosody
-c = ""
+c = "/home/dekel/Documents/myprosody/myprosody/dataset/audioFiles/"
+import os
 
+list = os.listdir(c) # dir is your directory path
+number_files = len(list)
 
-mysp.mysptotal(p,c)
-mysp.myspf0mean(p,c)
-mysp.myspf0sd(p,c)
-mysp.myspf0med(p,c)
-mysp.myspf0min(p,c)
-mysp.myspf0max(p,c)
-mysp.myspf0q25(p,c)
-mysp.myspf0q75(p,c)
+for i in range(number_files):
+    mysp.mysptotal(p + str(i),c)
+    mysp.myspf0mean(p + str(i),c)
+    mysp.myspf0sd(p + str(i),c)
+    mysp.myspf0med(p + str(i),c)
+    mysp.myspf0min(p + str(i),c)
+    mysp.myspf0max(p + str(i),c)
+    mysp.myspf0q25(p + str(i),c)
+    mysp.myspf0q75(p + str(i),c)
--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import myprosody as mp
 from pydub import AudioSegment
 p = ""
 c = "SegmentsLesson1"
-windowLength = 3000
+windowLength = 20000
 currentWindow = 0
 Audio = AudioSegment.from_wav("SSP frikandel Saar.wav")
 
 cnt = 0
 while currentWindow <= len(Audio):
-    newAudio = Audio[currentWindow:currentWindow + currentWindow]
+    newAudio = Audio[currentWindow:currentWindow + windowLength]
     currentWindow += windowLength
     newAudio.export("dataset" + "/" + "audioFiles" + "/" + c + "/audioSeg-" + str(cnt) + ".wav", format="wav")
     cnt+=1
