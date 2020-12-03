4afbadf02ace5cc839a8fa4658bac5cbde732218
Avinash Saravanan <asarav@umich.edu>
2020-12-03 15:16:21 +0100

    Update feature extraction to use parselmouth instead

--- extract_features.py
+++ extract_features.py
@@ -1,22 +1,31 @@
-#mysp = __import__("my-voice-analysis")
-import myprosody as mysp
-import pickle
+import parselmouth
 
-p = "audioSeg-"
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-#Insert your local directory address here to work with myprosody
-c = "/home/dekel/Documents/myprosody/myprosody/dataset/audioFiles/"
-import os
+sns.set() # Use seaborn's default style to make attractive graphs
 
-list = os.listdir(c) # dir is your directory path
-number_files = len(list)
+# Plot nice figures using Python's "standard" matplotlib library
+snd = parselmouth.Sound("dataset/audioFiles/SegmentsLesson1/audioSeg-1.wav")
+plt.figure()
+plt.plot(snd.xs(), snd.values.T)
+plt.xlim([snd.xmin, snd.xmax])
+plt.xlabel("time [s]")
+plt.ylabel("amplitude")
+plt.show()
 
-for i in range(number_files):
-    mysp.mysptotal(p + str(i),c)
-    mysp.myspf0mean(p + str(i),c)
-    mysp.myspf0sd(p + str(i),c)
-    mysp.myspf0med(p + str(i),c)
-    mysp.myspf0min(p + str(i),c)
-    mysp.myspf0max(p + str(i),c)
-    mysp.myspf0q25(p + str(i),c)
-    mysp.myspf0q75(p + str(i),c)
+print("Amplitude")
+print(snd.values.T)
+
+pitch = snd.to_pitch()
+pitch_values = pitch.selected_array['frequency']
+pitch_values[pitch_values==0] = np.nan
+plt.plot(pitch.xs(), pitch_values, 'o', markersize=5, color='w')
+plt.plot(pitch.xs(), pitch_values, 'o', markersize=2)
+plt.grid(False)
+plt.ylim(0, pitch.ceiling)
+plt.ylabel("fundamental frequency [Hz]")
+plt.show()
+print("Pitch")
+print(pitch_values)
